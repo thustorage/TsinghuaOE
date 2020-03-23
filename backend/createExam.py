d3284a13fc277e40f040a3e8037c54c3b3763301
neolinsu <neo.jia.lin@outlook.com>
2020-03-23 19:46:45 +0800

    fix typo ^ -> **

--- backend/createExam.py
+++ backend/createExam.py
@@ -25,7 +25,7 @@ def chr_width(c):
 
 def token_gen(stu_id, exam_name):
     m = hashlib.md5()
-    seed = random.randint(0, 2^24)
+    seed = random.randint(0, 2**24)
     m.update(str(seed).encode('utf-8'))
     m.update(stu_id.encode('utf-8'))
     m.update(exam_name.encode('utf-8'))
@@ -82,7 +82,6 @@ def exam_gen(r, stu_id, token, dist, items, problems):
         for i, s_problem in enumerate(s_problems):
             s_problem_options = []
             if 'options' in s_problem:
-                print(s_problem)
                 s_problem_options = s_problem['options']
             if 'rand_options' in item and item['rand_options']:
                 s_problem_options = random.sample(s_problem_options, len(s_problem_options))
@@ -100,7 +99,6 @@ def exam_gen(r, stu_id, token, dist, items, problems):
             ]
 
             id += 1
-    print(dist_list)
     assert r.set(token, json.dumps({'items': dist_list}))
 
 def md5(text):
@@ -127,44 +125,3 @@ for stu_id in stu_info:
     ori_dit = json.loads(r.get(md5(stu_id)))
     ori_dit['token'] = stu_token
     r.set(md5(stu_id), json.dumps(ori_dit))
-    print(r.get(md5(stu_id)))
-
-
-# template = {
-#     'id': 'bc75782a480fd7edcfe3ea7f65fb9a19',
-#     'password': '61e32b0f0aebe08825b25346dc5ae962',
-#     'token': '61e32b0f0aebe08825b25346dc5ae962'}
-# r.set('bc75782a480fd7edcfe3ea7f65fb9a19', json.dumps(template))
-# 
-# exam_for_id = {
-#     'items': [
-#         {'id':1,
-#          'label': "多项选择答案",
-#          'type': "select",
-#          'path': '',
-#          'options': ["Option A","Option B","Option C","Option D"],
-#          'multiple': True,
-#         },
-#         {'id':2,
-#          'type': "select",
-#          'label': "单项选择答案",
-#          'path': '',
-#          'options': ["Option A","Option B","Option C","Option D"],
-#          'multiple': False
-#         },
-#         {'id':3,
-#          'type': "text",
-#          'body': "test2",
-#          'path': ''
-#         },
-#         {'id':4,
-#          'type': "submit",
-#          'title': "提交",
-#          'body':"请确认所有题目已经完成作答。"
-#         }
-#       ]
-# }
-# r.set('61e32b0f0aebe08825b25346dc5ae962', json.dumps(exam_for_id))
-# print(json.dumps(exam_for_id))
-# print(json.loads(r.get('61e32b0f0aebe08825b25346dc5ae962')))
-
